428881d87ca7938759a8f8d43865e58b47477c5c
ChanJuShin <ckswn1259@naver.com>
2021-02-07 14:12:00 +0900

    useMemo 를 사용하여 연산한 값 재사용

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useMemo } from 'react';
 import './App.css';
 import UserList from "./UserList";
 import CreateUser from "./CreateUser";
+
+function countActiveUsers(users) {
+  console.log('활성 사용자 수를 세는중...');
+  return users.filter(user => user.active).length;
+}
 
 function App() {
   const [inputs, setInputs] = useState({
@@ -61,6 +66,7 @@ function App() {
       users.map(user => user.id === id ? { ...user, active: !user.active } : user)
     );
   };
+  const count = useMemo(() => countActiveUsers(users), [users]);
   return (
     <>
       <CreateUser
@@ -70,6 +76,7 @@ function App() {
         onCreate={onCreate}
       />
       <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
+      <div>활성사용자 수 : {count}</div>
     </>
   );
 }
